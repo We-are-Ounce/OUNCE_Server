a3673d499c35730052d528ab9014c4263dab75e6
최정균 <wjdrbs966@naver.com>
2020-07-04 16:42:45 +0900

    feature jg branch make

--- ounce/routes/index.js
+++ ounce/routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/search', require('./search'));
 
 module.exports = router;
