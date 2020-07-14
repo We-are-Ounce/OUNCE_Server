86f2fb5f87773a82f698240c4f4c84676a9efe75
정균 <wjdrbs966@naver.com>
2020-07-14 19:56:47 +0900

    check

--- ounce/controllers/profileController.js
+++ ounce/controllers/profileController.js
@@ -32,6 +32,12 @@ module.exports = {
             profileInfo,   
         } = req.body;
 
+        console.log(profileName)
+        console.log(profileWeight)
+        console.log(profileImg)
+        console.log(profileAge)
+        console.log(profileInfo);
+
 
         if (!userIdx) {
             res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST.resMessage.EMPTY_TOKEN));
