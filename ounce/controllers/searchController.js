658ca5fd2ac1eb39dc6263815205e5c15eb9274f
정균 <wjdrbs966@naver.com>
2020-07-15 19:42:01 +0900

    search log test

--- ounce/controllers/searchController.js
+++ ounce/controllers/searchController.js
@@ -16,6 +16,9 @@ const search = {
 
     searchFood: async(req, res) => {
         let {searchKeyword, pageStart, pageEnd} = req.body;
+        console.log(searchKeyword);
+        console.log(pageStart)
+        console.log(pageEnd)
 
         if (!searchKeyword || !pageStart || !pageEnd) {
             res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
