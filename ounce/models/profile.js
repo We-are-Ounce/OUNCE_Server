8d1dd654f862cbebd64270fccce6253cf0151d90
정균 <wjdrbs966@naver.com>
2020-07-17 00:01:05 +0900

    update profile query

--- ounce/models/profile.js
+++ ounce/models/profile.js
@@ -15,7 +15,7 @@ const profile = {
     },
 
     diffReviewAll: async (profileIdx, pageStart, pageEnd) => {
-        const query = `SELECT review.reviewIdx, food.foodIdx, food.foodImg, food.foodManu, food.foodName, review.reviewInfo, review.reviewRating, review.reviewPrefer, review.createdAt FROM review join food on review.foodIdx = food.foodIdx where review.profileIdx="${profileIdx}" LIMIT ${pageStart}, ${pageEnd};` 
+        const query = `SELECT review.reviewIdx, food.foodIdx, food.foodImg, food.foodManu, food.foodName, review.reviewInfo, review.reviewRating, review.reviewPrefer, review.createdAt FROM review join food on review.foodIdx = food.foodIdx where review.profileIdx="${profileIdx}" LIMIT ${pageStart}, ${pageEnd}`; 
         try {
             const result = await pool.queryParamArr(query);
             return result;
