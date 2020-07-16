0feac56c04df5ef1fecb27e3c8c425585f39e428
정균 <wjdrbs966@naver.com>
2020-07-16 16:11:33 +0900

    log test

--- ounce/controllers/searchController.js
+++ ounce/controllers/searchController.js
@@ -49,13 +49,12 @@ const search = {
     toWrite: async(req, res) => {
         const profileIdx = req.params.profileIdx;
         const {searchKeyword, pageStart, pageEnd} = req.body;
+        console.log(profileIdx);
+        console.log(searchKeyword);
+        console.log(pageStart);
+        console.log(pageEnd);
 
-        if (!profileIdx) {
-            res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
-            return;
-        }
-
-        if (!searchKeyword || !pageStart || !pageEnd) {
+        if (!profileIdx || !searchKeyword || !pageStart || !pageEnd) {
             res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
             return;
         }
