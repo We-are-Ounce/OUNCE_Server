75b09f23491122a700cae7fb5f4c2ba556b05ba1
hyowon <dndwls9252@naver.com>
2020-07-16 13:36:27 +0900

    add foodImg

--- ounce/models/search.js
+++ ounce/models/search.js
@@ -71,7 +71,7 @@ const search = {
     },
 
     reviewALL: async(foodIdx) => {
-        const query = `SELECT p.profileIdx, p.profileName, p.profileInfo, p.profileAge, r.reviewIdx, r.reviewRating, r.reviewPrefer, f.foodIdx FROM profile p JOIN review r ON p.profileIdx = r.profileIdx JOIN food f ON f.foodIdx = r.foodIdx WHERE f.foodIdx = ${foodIdx}`;
+        const query = `SELECT p.profileIdx, r.foodImg, p.profileName, p.profileInfo, p.profileAge, r.reviewIdx, r.reviewRating, r.reviewPrefer, f.foodIdx FROM profile p JOIN review r ON p.profileIdx = r.profileIdx JOIN food f ON f.foodIdx = r.foodIdx WHERE f.foodIdx = ${foodIdx}`;
         try {
             const result = await pool.queryParam(query);
             return result;
