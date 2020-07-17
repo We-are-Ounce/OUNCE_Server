076d02918283489c24cc425630c5651933a52840
정균 <wjdrbs966@naver.com>
2020-07-18 02:26:48 +0900

    log delete

--- ounce/controllers/reviewController.js
+++ ounce/controllers/reviewController.js
@@ -15,9 +15,6 @@ module.exports = {
         // 리뷰 (평점, 선호도, 한줄소개, 변상태, 변냄새, 트리블(눈, 귀, 털, 구토), 메모
         const {reviewRating, reviewPrefer, reviewInfo, reviewMemo, reviewStatus, reviewSmell, reviewEye, reviewEar, reviewHair, reviewVomit, foodIdx, profileIdx} = req.body;
 
-        console.log(profileIdx);
-        console.log(foodIdx);
-
         if (!userIdx) {
             res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST), resMessage.EMPTY_TOKEN);
             return;
